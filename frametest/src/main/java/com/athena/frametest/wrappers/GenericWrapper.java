1b4c18e6362e4205e56a64920f81dbc7881a01e5
djagand <30496465+djagand@users.noreply.github.com>
2017-08-07 17:36:05 +0530

    commit

--- frametest/src/main/java/com/athena/frametest/wrappers/GenericWrapper.java
+++ frametest/src/main/java/com/athena/frametest/wrappers/GenericWrapper.java
@@ -2,4 +2,136 @@ package com.athena.frametest.wrappers;
 
 public class GenericWrapper implements wrappers{
 
+	@Override
+	public void invokeApp(String browser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterById(String idValue, String data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterByName(String nameValue, String data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enterByXpath(String xpathValue, String data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean verifyTitle(String title) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void verifyTextById(String id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void verifyTextByXpath(String xpath, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void verifyTextContainsByXpath(String xpath, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void verifyTextContainsById(String id, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clickById(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clickByClassName(String classVal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clickByName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clickByLink(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clickByXpath(String xpathVal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTextById(String idVal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTextByXpath(String xpathVal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void selectVisibileTextById(String id, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectIndexById(String id, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void switchToParentWindow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void switchToLastWindow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void acceptAlert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quitBrowser() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
