06702eaae4905ced3a760dbbd3a87741349b6eb2
djagand <30496465+djagand@users.noreply.github.com>
2017-08-22 18:24:46 +0530

    new files

---
+++ frametest/src/main/java/com/athena/cybertron/utils/CommonUtils.java
@@ -0,0 +1,24 @@
+package com.athena.cybertron.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class CommonUtils {
+
+	public static Properties getConfigProperties() {
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream(new File("./src/main/java/config.properties")));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return prop;
+	}
+}
