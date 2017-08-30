245b106633d4095bfb63f49d3c5410e3a98bd9d6
vdevishri <30524154+vdevishri@users.noreply.github.com>
2017-08-30 12:00:29 +0530

    Invalid Login Page

---
+++ cybertron/src/test/java/com/athena/cybertron/pages/InvalidLoginPage.java
@@ -0,0 +1,9 @@
+package com.athena.cybertron.pages;
+
+import java.util.Properties;
+
+import com.athena.cybertron.wrappers.FacebookWrapper;
+
+public class InvalidLoginPage extends FacebookWrapper {
+	public Properties invalidLoginPageProp = loadProp("./pageObjects/invalidLoginPage.properties");
+}
